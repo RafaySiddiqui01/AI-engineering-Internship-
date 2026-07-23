62074d94ac2cac4afec9d03b4ee99f29b2c0a9c2
RafaySiddiqui01 <siddiquirafay01@gmail.com>
2026-07-23 14:52:50 +0500

    Initial commit

---
+++ intro.py
@@ -0,0 +1,4 @@
+print("Hello! My name is Rafay Siddiqui.")
+print("I am a final-year Computer Science student.")
+print("I am interested in Artificial Intelligence, Machine Learning, and Software Development.")
+print("I enjoy building real-world projects and learning new technologies.")
